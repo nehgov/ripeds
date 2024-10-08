2c138eb775a414e9240b711d88cd06a70e87f465
Benjamin Skinner <bskinner@neh.gov>
2024-10-08 17:01:14 -0400

    added html parser

--- data-raw/make_ipeds_dict.R
+++ data-raw/make_ipeds_dict.R
@@ -19,11 +19,29 @@ parse_xlsx <- function(zipfile, dfile) {
   ## return
   out
 }
-parse_html <- function(zip, dfile) {
+parse_html <- function(zipfile, dfile) {
+  ## unzip file
+  f <- unzip(zipfile, dfile, exdir = tempdir())
+  ## read in html
+  f <- readLines(hfile)
+  ## strip unicode
+  f <- iconv(f, "latin1", "ascii", sub = "")
+  ## keep only those that:
+  ## 1. start with text
+  ## 2. start with <br><br>TEXT and end with <hr>
+  f <- grep("^[A-Z0-9].+-[0-9]+-[A-Za-z].+$|<br><br>[A-Z].+<hr>", f, value = TRUE)
+  ## pull out relevant variable name for the <br><br> values
+  f <- gsub("^<br><br>.+<hr>(.+)$", "\\1", f)
+  ## split
+  varname <- gsub("^([A-Z]+_?[A-Z]*[0-9]*[A-Z]*)-.+$", "\\1", f)
+  vartitle <- gsub("^([A-Z]+_?[A-Z]*[0-9]*[A-Z]*)-([0-9]*)?-?([Imputation]*.+)$", "\\3", f)
+  ## put into tibble
+  dplyr::tibble(varname = varnames,
+                vartitle = vartitle)
 }
 
 ## get list of available IPEDS dictionary files
-itab <- ripeds::ipeds_file_table() |> filter(year %in% 2004:2023)
+itab <- ripeds::ipeds_file_table() |> dplyr::filter(year %in% 2000:2000)
 
 ## init list of proper size
 dict <- vector("list", length = nrow(itab))
@@ -53,4 +71,4 @@ for (i in 1:length(dict)) {
 }
 
 ## bind
-dict <- bind_rows(dict)
+dict <- dplyr::bind_rows(dict)
