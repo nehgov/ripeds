ad34236ffea3d1a0c4d8e8c83d3844ad351aa584
Benjamin Skinner <bskinner@neh.gov>
2024-10-18 12:10:59 -0400

    catching up work

--- R/ipeds_to_disk.R
+++ R/ipeds_to_disk.R
@@ -47,3 +47,46 @@ ipeds_tmp_to_disk <- function(to_dir, overwrite_existing = FALSE,
     invisible(file.remove(file.path(tempdir(), files_to_save)))
   }
 }
+
+#' Download IPEDS files directly to disk.
+#'
+#' Directly download and save IPEDS zip files to disk for future usage. By
+#' default, the function will create directories as necessary to save the files.
+#'
+#' @param files Vector of files (without ending) to download (e.g., HD2023)
+#' @param to_dir Directory path to copy
+#' @param overwrite_existing Overwrite files on local directory with those in
+#'   temporary directory
+#' @param create_directory Recursively create directory path specified by user
+#'   if it does not exist
+#'
+#' @export
+ipeds_download_to_disk <- function(files,
+                                   to_dir,
+                                   overwrite_existing = FALSE,
+                                   create_directory = TRUE) {
+  base_url <- "https://nces.ed.gov/ipeds/datacenter/data"
+  ## check path and create if doesn't exist (or give warning if user sets
+  ## create_directory = FALSE
+  if (!dir.exists(to_dir)) {
+    if (create_directory) {
+      dir.create(to_dir, showWarnings = FALSE, recursive = TRUE)
+    } else {
+      stop("Directory does not exist. ",
+           "Either create directory path or set create_directory = TRUE")
+    }
+  }
+  ## append ending to files
+  fzipvec <- paste0(files, ".zip")
+  ## loop through
+  for (f in fzipvec) {
+    if (!file.exists(file.path(to_dir, f))) {
+      ## download to local directory
+      download.file(file.path(base_url, f),
+                    file.path(to_dir, f),
+                    quiet = TRUE, mode = "wb")
+      ## add counter so not throttled
+      if (i %% 50 == 0) Sys.sleep(10)
+    }
+  }
+}
