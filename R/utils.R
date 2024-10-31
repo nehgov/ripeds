f7334d6897ae602150cfa30c008d0a47cd38b7ef
Benjamin Skinner <bskinner@neh.gov>
2024-10-31 09:39:46 -0400

    added year conversion function

--- R/utils.R
+++ R/utils.R
@@ -10,3 +10,8 @@ hline <- function(nchar, symbol = "-") {
 get_hash <- function(x, hash_env) {
   sapply(x, FUN = get, envir = hash_env)
 }
+
+## convert calendar year (YYYY) to academic year (YY/YY+1)
+cyear_to_ayear <- function(x) {
+  paste0(substr(x, 3, 4), substr(x + 1, 3, 4))
+}
