70455e5e716ee3286e1ae3ae2e2dd044770dfc7f
Benjamin Skinner <bskinner@neh.gov>
2024-12-05 10:15:00 -0500

    adjust args slightly and add roxygen

--- R/init.R
+++ R/init.R
@@ -3,19 +3,25 @@
 #' This function initializes the data request. It should always be
 #' the first in the series of piped functions.
 #'
+#' @param local_dir Local directory to check for complete IPEDS zip files that
+#'   have already been downloaded. Newly downloaded files will be placed here as
+#'   well. If unset, all files will be stored in tempdir().
+#' @param use_revised_files When IPEDS zip files contain revised data, use the
+#'   revised files. Default is TRUE.
+#'
 #' @examples
 #' \dontrun{
 #' ipeds_init()
 #' }
 
 #' @export
-ipeds_init <- function(local_dir = NA, revised_files = TRUE) {
+ipeds_init <- function(local_dir = NA, use_revised_files = TRUE) {
   ## run ipeds_file_table() to bring into memory
   ipeds_file_table()
   ## set up list for chained call
   list("ipeds_init_list" = TRUE,
        "local_dir" = local_dir,
-       "revised_files" = revised_files,
+       "revised_files" = use_revised_files,
        "dict" = NULL,
        "survey" = NULL,
        "select" = NULL,
