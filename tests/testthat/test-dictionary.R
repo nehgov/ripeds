7a3551c73dcedcfa3bf4844658056b6d862bbf77
Benjamin Skinner <bskinner@neh.gov>
2025-02-14 11:51:12 -0500

    added tests for dictionary

--- tests/testthat/test-dictionary.R
+++ tests/testthat/test-dictionary.R
@@ -2,14 +2,18 @@
 ## -- dictionary --
 ## -----------------------------------------------------------------------------
 
-
 test_that("Dictionary does not return correct object", {
-  varn <- ipeds_dict("stabbr", exact_match = TRUE, return_dict = TRUE, print_off = TRUE)[1, "varname"]
-  expect_identical("stabbr", varn)
+  dict <- ipeds_dict("stabbr", exact_match = TRUE, return_dict = TRUE,
+                     print_off = TRUE)
+  expect_identical(dict[["varname"]], rep("stabbr", nrow(dict)))
+  expect_equal(any(grepl("HD2002", dict[["filename"]])), TRUE)
+  expect_equal(dict[dict[["filename"]] == "HD2023", "description"],
+               "State abbreviation")
 })
 
 test_that("Error for non-match", {
-  expect_error(ipeds_dict("stabbrr", exact_match = TRUE, return_dict = TRUE, print_off = TRUE),
+  expect_error(ipeds_dict("stabbrr", exact_match = TRUE, return_dict = TRUE,
+                          print_off = TRUE),
                "No matches! Try again with new string or column.")
   expect_error(ipeds_dict("stabbrr", return_dict = TRUE, print_off = TRUE),
                "No matches! Try again with new string or column.")
