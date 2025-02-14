af53ec331813e7536890664a8ef829b870a99af5
Benjamin Skinner <bskinner@neh.gov>
2025-02-14 11:18:37 -0500

    added filter and init tests

--- tests/testthat/test-filter.R
+++ tests/testthat/test-filter.R
@@ -14,6 +14,10 @@ dil <- list("ipeds_init_list" = TRUE,
             "year" = NULL,
             "nse" = TRUE)
 
+## -----------------------------------------------------------------------------
+## errors
+## -----------------------------------------------------------------------------
+
 test_that("Errors for non-init()", {
   expect_error(ipeds_filter(unitid == 99999),
                "Chain not properly initialized. Be sure to start with ipeds_init().")
@@ -25,4 +29,47 @@ test_that("Error for bad variable names", {
                      "Please check your spelling or search dictionary:",
                      "?ipeds_dict()"),
                fixed = TRUE)
+})
+
+## -----------------------------------------------------------------------------
+## nse filters
+## -----------------------------------------------------------------------------
+
+test_that("Simple filter is parsed", {
+  call <- ipeds_filter(dil, stabbr == "KY")
+  expect_equal(is.name(call$fvars[[1]]), TRUE)
+  expect_equal(call$fvars[[1]], as.name("stabbr"))
+  expect_equal(deparse(call$filter[[1]]), "stabbr == \"KY\"")
+})
+
+test_that("Double filter is parsed", {
+  call <- ipeds_filter(dil, stabbr == "KY", control %in% c(1:2))
+  expect_equal(length(call$fvars), 2)
+  expect_equal(is.name(call$fvars[[1]]), TRUE)
+  expect_equal(is.name(call$fvars[[2]]), TRUE)
+  expect_equal(call$fvars[[1]], as.name("stabbr"))
+  expect_equal(call$fvars[[2]], as.name("control"))
+  expect_equal(deparse(call$filter[[1]]), "stabbr == \"KY\"")
+  expect_equal(deparse(call$filter[[2]]), "control %in% c(1:2)")
+})
+
+## -----------------------------------------------------------------------------
+## string filters
+## -----------------------------------------------------------------------------
+
+test_that("Simple string filter is parsed", {
+  call <- ipeds_filter(dil, "stabbr == 'KY'")
+  expect_equal(is.character(call$fvars[[1]]), TRUE)
+  expect_equal(call$fvars[[1]], "stabbr")
+  expect_equal(deparse(call$filter[[1]]), "stabbr == \"KY\"")
+})
+
+test_that("Double string filter is parsed", {
+  call <- ipeds_filter(dil, "stabbr == 'KY' | control %in% c(1:2)")
+  expect_equal(length(call$fvars), 2)
+  expect_equal(is.character(call$fvars[[1]]), TRUE)
+  expect_equal(is.character(call$fvars[[2]]), TRUE)
+  expect_equal(call$fvars[[1]], "stabbr")
+  expect_equal(call$fvars[[2]], "control")
+  expect_equal(deparse(call$filter[[1]]), "stabbr == \"KY\" | control %in% c(1:2)")
 })
